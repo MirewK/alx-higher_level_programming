ff6febaf3c2ea9896535076514d00354ab7bbd49
MirewK <meremehertab@gmail.com>
2024-01-08 13:02:11 -0800

    python data structure

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for x in range(len(my_list)):
+        if my_list.append(True)
+        else:
+            new_list.append(False)
+            return new_list
