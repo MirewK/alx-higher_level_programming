9645cd51e0ebfd9a2c4794af6e81696e312f048c
MirewK <meremehertab@gmail.com>
2024-01-08 05:06:32 -0800

    print list integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list = []):
-    print(*my_list, sep = "\n")
-   # my_list = [1, 2, 3, 4, 5]
+    print(my_list, sep = "\n")
     ptint_list_integer(my_list)
