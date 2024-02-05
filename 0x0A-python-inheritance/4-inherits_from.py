a20a62824101a007c996122eb5bf0a645488c2dd
MirewK <meremehertab@gmail.com>
2024-02-05 09:59:34 -0800

    inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a function that checks an inherited class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): object to be checked
+        a_class (type): class type to be mached with object
+    Returns:
+        True if obj is an inherited instance of a_class
+        False otherwise
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
