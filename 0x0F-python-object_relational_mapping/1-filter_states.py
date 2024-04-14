bf5085bde15edc5beb81d9f43c30726c381b980f
MirewK <meremehertab@gmail.com>
2024-04-14 12:15:02 -0700

    python object relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,9 +4,15 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    #Gets credentials from command-line arg and connect to MySQL server
-    db == MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    """
+    Gets credentials from command-line arg and connect to MySQL server
+    """
+    db == MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
 
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    c.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
