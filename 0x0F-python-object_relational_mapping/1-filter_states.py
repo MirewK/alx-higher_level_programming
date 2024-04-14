f4079cdf122cab0dd046bca365787bf36b9e2005
MirewK <meremehertab@gmail.com>
2024-04-14 12:00:57 -0700

    python object relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     #Gets credentials from command-line arg and connect to MySQL server
     db == MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
+    c = db.cursor()
 
-    cursor.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in cursor.fetchall() if state[1][0] == "N"]
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
